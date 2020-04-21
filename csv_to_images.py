77b129b4b7d84e9ab951976c340a4cfeaebaacd5
Harsh Khandelwal <49086820+because-i-m-batman@users.noreply.github.com>
2020-04-21 15:19:43 +0530

    Update csv_to_images.py

--- csv_to_images.py
+++ csv_to_images.py
@@ -1,45 +1,28 @@
-import os
-import sys
-import cv2
 import numpy as np
+import cv2
+import os
 
+os.mkdir("output")
 
-def main():
+with open("train.csv") as f:
+    content = f.readlines()
 
-if len(sys.argv) < 2:
+lines = np.array(content)
 
-    print ('Usage: python cv_to_img.py [output_path]')
+num_of_instances = lines.size
 
-return -1
+for i in range(1,num_of_instances):
+    try:
+        emotion, img = lines[i].split(",")
+        img = img.replace('"', '')
+        img = img.replace('\n', '')
+        pixels = img.split(" ")
 
-output_path = sys.argv[1]`
+        pixels = np.array(pixels, 'float32')
+        image = pixels.reshape(48, 48)
 
-if os.path.exists(output_path):
+        path_file_name = f"output/{i}_{emotion}.jpg"
+        cv2.imwrite(path_file_name, image)
 
-    os.system('rm -rf {}'.format(output_path))
-
-os.system('mkdir {}'.format(output_path))
-
-label_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
-
-data = np.genfromtxt('fer2013.csv',delimiter=',',dtype=None)
-
-labels = data[1:,0].astype(np.int32)
-image_buffer = data[1:,1]
-images = np.array([np.fromstring(image, np.uint8, sep=' ') for image in image_buffer])
-usage = data[1:,2]
-dataset = zip(labels, images, usage)
-for i, d in enumerate(dataset):
-    usage_path = os.path.join(output_path, d[-1])
-    label_path = os.path.join(usage_path, label_names[d[0]])
-    img = d[1].reshape((48,48))
-    img_name = '%08d.jpg' % i
-    img_path = os.path.join(label_path, img_name)
-    if not os.path.exists(usage_path):
-        os.system('mkdir {}'.format(usage_path))
-    if not os.path.exists(label_path):
-        os.system('mkdir {}'.format(label_path))
-    cv2.imwrite(img_path, img)
-    print 'Write {}'.format(img_path)
-if __name__ == '__main__':
-    main()
+    except Exception as ex:
+        print(ex)
